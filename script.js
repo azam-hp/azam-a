e52d05278441f88c900f83e582fc2f29a4cff1b7
azam-hp <a98371489@gamil.com>
2025-02-16 18:59:22 +0500

    -

---
+++ script.js
@@ -0,0 +1,25 @@
+const tableView = document.querySelector("table");
+const cardView = document.querySelector(".task-container");
+const showTableBtn = document.querySelector(".box24");
+const showCardsBtn = document.querySelector(".box2");
+
+tableView.style.display = "table";
+cardView.style.display = "none";
+showTableBtn.style.color = "blue"; 
+showCardsBtn.style.color = "gray";  
+
+showTableBtn.onclick = function () {
+    tableView.style.display = "table";
+    cardView.style.display = "none";
+
+    showTableBtn.style.color = "blue";  
+    showCardsBtn.style.color = "gray"; 
+};
+
+showCardsBtn.onclick = function () {
+    tableView.style.display = "none";
+    cardView.style.display = "grid";
+
+    showTableBtn.style.color = "gray"; 
+    showCardsBtn.style.color = "blue"; 
+};
